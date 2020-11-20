35f854d379ed70c18fbe60de9cffee65cc233326
Serge Ko <newkozlukov@gmail.com>
2020-11-20 03:42:17 +0300

    disable wayland stuff
    1) the config hardcoded wayland at compile-time 2) I'm switching to X
    again

--- home/intm.nix
+++ home/intm.nix
@@ -21,22 +21,18 @@
   };
   services.redshift = {
     enable = true;
-    package = pkgs.redshift-wlr;
+    # extraOptions = [ "-m wayland" ];
+    # package = pkgs.redshift-wlr;
     latitude = "55.6765651";
     longitude = "37.7623706";
     temperature = {
       day = 6000;
       night = 2100;
     };
-    extraOptions = [ "-m wayland" ];
   };
 
   home.sessionVariables = {
     MOZ_USE_XINPUT2 = 1;
-    MOZ_ENABLE_WAYLAND = 1;
     QT_WAYLAND_FORCE_DPI = "physical";
-    GDK_BACKEND = "wayland";
-    SDL_VIDEODRIVER = "wayland";
-    CLUTTER_BACKEND = "wayland";
   };
 }
