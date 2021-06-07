8a4770dd4e98e8c783cbcf0cee7cac5375616375
Someone Serge <sergei.kozlukov@aalto.fi>
2021-06-07 18:32:36 +0300

    vpn-slice works
    overlay vpn-slice to use an arbitrary commit from the master branch
    after the fix for read-only hosts has been applied

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,10 +5,23 @@ let
     (final: prev: { inherit (nixGL) nixGLNvidia nixGLIntel nixGLDefault; });
   useModernNix = (final: prev: { nixModern = nix.packages.${system}.nix; });
   addPythonLinters = (import ./pylinters.nix);
+  vpnSliceReadonlyHosts = (final: prev: {
+    vpn-slice = prev.vpn-slice.overrideDerivation (oldAttrs:
+      with oldAttrs; rec {
+        version = "0.15";
+        src = final.fetchFromGitHub {
+          owner = "dlenski";
+          repo = pname;
+          rev = "bf445f33f73c0a93745920c8cd46753b529923c8";
+          sha256 = "Jk/qiMhBvaeqPFe1Xe3WMF/f4WoHfkjy+L0FoZPhYL4=";
+        };
+      });
+  });
   overlays = [
     addInjectGL
     # useNixUnstable  -- this breaks cachix
     addPythonLinters
     useModernNix
+    vpnSliceReadonlyHosts
   ];
 in overlays
