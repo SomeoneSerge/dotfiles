8ad2bc792a53139272d598907952d86c0ec3b4ad
Someone S <newkozlukov@gmail.com>
2021-08-10 03:27:04 +0300

    lite21: try to fix headers for aziz's calibre
    auth headers of calibre were getting lost smhw (maybe still do)

--- hosts/lite21/aziz-calibre.nix
+++ hosts/lite21/aziz-calibre.nix
@@ -6,11 +6,17 @@ let
     "/calibre" = {
       proxyPass = "http://${hosts.aziz-thinkpad.wireguard.address}:8083";
       extraConfig = ''
-        proxy_bind              10.24.60.1;
-        proxy_set_header        Host            $host;
-        proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header        X-Scheme        $scheme;
-        proxy_set_header        X-Script-Name   /calibre;  # IMPORTANT: path has NO trailing slash
+        # proxy_bind              10.24.60.1;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header Host            $host;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Scheme        $scheme;
+        proxy_set_header X-Script-Name   /calibre;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Forwarded-Ssl on;
+        proxy_hide_header 'x-frame-options';
+        add_header x-frame-options "allowall";
+        proxy_read_timeout 90;
       '';
     };
   };
