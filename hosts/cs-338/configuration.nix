e708dc17692ae8eed879c95b3b81f3cc9edccffb
Someone Serge <sergei.kozlukov@aalto.fi>
2021-08-11 16:49:49 +0300

    cs-338: gdm disable autoSuspend

--- hosts/cs-338/configuration.nix
+++ hosts/cs-338/configuration.nix
@@ -88,6 +88,7 @@
   # Enable the Plasma 5 Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.autoSuspend = false;
 
   services.haveged.enable = true;
 
