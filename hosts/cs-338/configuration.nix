66f63a404d9df03890e64d59f3fc5c31c819430a
Someone Serge <sergei.kozlukov@aalto.fi>
2021-08-11 19:21:49 +0300

    cs-338: alacritty and gnome-keyring to sometime move to sway

--- hosts/cs-338/configuration.nix
+++ hosts/cs-338/configuration.nix
@@ -144,6 +144,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    alacritty
     git
     nixfmt
     wget
@@ -189,6 +190,7 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
+  services.gnome.gnome-keyring.enable = true;
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
