8f19ed18aca803c70af971357f10344bb7289c44
Someone Serge <sergei.kozlukov@aalto.fi>
2021-10-12 03:42:21 +0300

    enable some modern-unix tools

---
+++ home/modern-unix.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, lib, ... }:
+
+let
+  inherit (lib) mkIf mkOption mkEnableOption mkDefault types;
+  cfg = config.home.modern-unix;
+in
+{
+  options.modern-unix = {
+    enable = mkOption {
+      description = ''
+        Enable modern busybox replacements as per
+        https://github.com/ibraheemdev/modern-unix
+      '';
+      type = types.bool;
+      default = true;
+    };
+  };
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      du-dust
+      duf
+      fd
+      ag
+      ripgrep
+      choose
+      gping
+      procs
+      dogdns
+    ];
+    programs.broot.enable = mkDefault true;
+    programs.bat.enable = mkDefault true;
+    programs.exa.enable = mkDefault true;
+    programs.lsd.enable = mkDefault true;
+    programs.git.delta.enable = mkDefault true;
+    programs.zoxide.enable = mkDefault true;
+  };
+}
