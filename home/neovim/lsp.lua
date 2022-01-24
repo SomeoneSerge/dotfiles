3cd74bb90bf8a5db7e35886254e1fc5499736a87
Someone Serge <sergei.kozlukov@aalto.fi>
2022-01-24 18:31:58 +0200

    lsp-config: bind formatting() unconditionally

--- home/neovim/lsp.lua
+++ home/neovim/lsp.lua
@@ -61,12 +61,14 @@ local on_attach = function(client, bufnr)
                    '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
 
     -- Set some keybinds conditional on server capabilities
-    if client.resolved_capabilities.document_formatting then
-        buf_set_keymap("n", "<leader>F",
-                       "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-    elseif client.resolved_capabilities.document_range_formatting then
+    if client.resolved_capabilities.document_range_formatting then
         buf_set_keymap("n", "<leader>F",
                        "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+    else
+    -- OLD: if client.resolved_capabilities.document_formatting then
+    -- NEW: bind \F unconditionally
+        buf_set_keymap("n", "<leader>F",
+                       "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
     end
 end
 
