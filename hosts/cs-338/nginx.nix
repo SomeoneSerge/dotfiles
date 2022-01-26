16b0ee96a7c9b8c78888bb1ecc9daa9107279c12
Someone Serge <sergei.kozlukov@aalto.fi>
2022-01-26 14:58:31 +0200

    cs-338: up nginx and route to jhub
    ...acme currently not working, need the public internet to resolve fqdn
    to a public ip (currently it resolves to a wireguard ip)

--- hosts/cs-338/nginx.nix
+++ hosts/cs-338/nginx.nix
@@ -1,15 +1,37 @@
 { config, pkgs, lib, ... }:
 let
   jhubDomain = "jhub.${config.networking.hostName}.someonex.net";
+  fqdn = "${config.networking.hostName}.someonex.net";
 in
 {
   services.nginx.enable = true;
 
-  networking.hosts."127.0.0.1" = [ jhubDomain ];
+  networking.hosts."127.0.0.1" = [ jhubDomain fqdn ];
+
+  security.acme.email = "sergei.kozlukov@aalto.fi";
+  security.acme.acceptTerms = true;
+  security.acme.certs."${fqdn}".extraDomainNames = [
+    jhubDomain
+  ];
+  systemd.services."acme-${fqdn}".enable = false;
+
+  services.nginx.recommendedTlsSettings = true;
+  services.nginx.recommendedOptimisation = true;
+  services.nginx.recommendedGzipSettings = true;
+  services.nginx.recommendedProxySettings = true;
+
   services.nginx.virtualHosts.${jhubDomain} = {
-    locations."/".proxyPass = "http://${config.services.jhub.host}:${toString config.services.jhub.port}";
+    forceSSL = true;
+    useACMEHost = fqdn;
+
+    locations."/".proxyPass = "http://${config.services.jhub.host}:${toString config.services.jhub.port}/";
+    locations."/".proxyWebsockets = true;
     listenAddresses =
       [ "127.0.0.1" ]
       ++ config.networking.wg-quick.interfaces.wg24601.address;
+  };
+  services.nginx.virtualHosts.${fqdn} = {
+    forceSSL = true;
+    enableACME = true;
   };
 }
