846fe2f02b35a830892f34608716defeb3826850
Someone Serge <sergei.kozlukov@aalto.fi>
2022-05-14 12:12:25 +0300

    neovim: new recommended keybindings

--- home/neovim/lsp.lua
+++ home/neovim/lsp.lua
@@ -20,58 +20,46 @@ cmp.setup({
     }
 })
 
-local on_attach = function(client, bufnr)
-    local function buf_set_keymap(...)
-        vim.api.nvim_buf_set_keymap(bufnr, ...)
-    end
-    local function buf_set_option(...)
-        vim.api.nvim_buf_set_option(bufnr, ...)
-    end
+local opts = {noremap = true, silent = true}
+vim.api.nvim_set_keymap('n', '<space>e',
+                        '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>',
+                        opts)
+vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>',
+                        opts)
+vim.api.nvim_set_keymap('n', '<space>q',
+                        '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
 
-    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+local on_attach = function(client, bufnr)
+    local function set_key(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+    local function set_opt(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+    set_opt('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
     -- Mappings.
-    local opts = {noremap = true, silent = true}
-    buf_set_keymap('n', '<leader>gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>',
-                   opts)
-    buf_set_keymap('n', '<leader>gd', '<Cmd>lua vim.lsp.buf.definition()<CR>',
-                   opts)
-    buf_set_keymap('n', '<leader>gt',
-                   '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    buf_set_keymap('n', '<leader>gr', '<cmd>lua vim.lsp.buf.references()<CR>',
-                   opts)
-    buf_set_keymap('n', '<leader>gi',
-                   '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-
-    buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    buf_set_keymap('n', '<leader>rd',
-                   '<cmd>lua vim.diagnostic.open_float()<CR>',
-                   opts)
-    buf_set_keymap('n', '<leader>rh', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    buf_set_keymap('n', '<leader>rs',
-                   '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    buf_set_keymap('n', '<space>wa',
-                   '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<space>wr',
-                   '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<space>wl',
-                   '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
-                   opts)
-    buf_set_keymap('n', '<leader>rk',
-                   '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-    buf_set_keymap('n', '<leader>rj',
-                   '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-    buf_set_keymap('n', '<leader>rl',
-                   '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-
-    buf_set_keymap("n", "<leader>F", "<cmd>lua vim.lsp.buf.formatting()<CR>",
-                   opts)
+    set_key('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    set_key('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    set_key('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    set_key('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    set_key('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    set_key('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>',
+            opts)
+    set_key('n', '<space>wr',
+            '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    set_key('n', '<space>wl',
+            '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
+            opts)
+    set_key('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    set_key('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    set_key('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    set_key('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    set_key('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 
     -- Set some keybinds conditional on server capabilities
-    if client.server_capabilities.document_range_formatting then
-        buf_set_keymap("v", "<leader>F",
-                       "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
-    end
+    -- if client.server_capabilities.document_range_formatting then
+    -- end
+    set_key("v", "<leader>F", "<cmd>lua vim.lsp.buf.range_formatting()<CR>",
+            opts)
 end
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp
