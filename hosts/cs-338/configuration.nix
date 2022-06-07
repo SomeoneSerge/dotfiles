0bfd0169ae6c2400b849336128d7fcdedd4db993
Someone Serge <sergei.kozlukov@aalto.fi>
2022-06-07 20:29:43 +0300

    cs-338: allow 12345 udp (generic, but now for gstreamer udpsrc)

--- hosts/cs-338/configuration.nix
+++ hosts/cs-338/configuration.nix
@@ -483,6 +483,7 @@ in
   services.openssh.forwardX11 = true;
   networking.firewall.enable = true;
   networking.firewall.trustedInterfaces = [ "wg24601" ];
+  networking.firewall.allowedUDPPorts = [ 12345 ];
   networking.firewall.allowedTCPPorts = [ 5201 ];
 
   networking.wg-quick.interfaces.wg24601 = {
